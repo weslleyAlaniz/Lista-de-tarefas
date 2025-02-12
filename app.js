210e35ef7c384b30cca6234cf25565a97b0f247f
weslleyAlaniz <weslley.alaniz40@gmail.com>
2025-02-12 16:25:26 -0300

    Lista de tarefas 0.5

---
+++ app.js
@@ -0,0 +1,12 @@
+function adicionarTarefa() {
+    let tarefa = document.querySelector("#task").value; 
+    let listaDeTarefas = document.getElementById("tasks");
+
+    listaDeTarefas.innerHTML += `
+        
+        <div class="task">
+            <input type="checkbox" class="checkbox">
+            <label class="checkmark"></label>
+            <span class="task-text">${tarefa}</span> <!-- Aqui usamos a variável tarefa -->
+        </div>`;
+    }
